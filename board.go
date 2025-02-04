dea9258a0e3d852e72ceac8e64e52e8c1c37c093
“Prajanya-Sharma” <prajanyasharma.bt23ele@pec.edu.in>
2025-02-04 10:16:19 +0530

    added peices to board

--- board.go
+++ board.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"image/color"
+	"path/filepath"
+	"strings"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
@@ -11,6 +13,33 @@ import (
 )
 
 const boardSize = 8
+const pieceDir = "chess-gui/peices"
+
+const startFenNotation = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+
+var mpPieceToImage = map[rune]string{
+	'P': "whitePawn.svg", 'N': "whiteKnight.svg", 'B': "whiteBishop.svg", 'R': "whiteRook.svg",
+	'Q': "whiteQueen.svg", 'K': "whiteKing.svg",
+	'p': "blackPawn.svg", 'n': "blackKnight.svg", 'b': "blackBishop.svg", 'r': "blackRook.svg",
+	'q': "blackQueen.svg", 'k': "blackKing.svg",
+}
+
+func parseFEN(fen string) [8][8]rune {
+	var board [8][8]rune
+	rows := strings.Split(fen, "/")
+	for rowIdx, row := range rows {
+		colIdx := 0
+		for _, char := range row {
+			if char >= '1' && char <= '8' {
+				colIdx += int(char - '0')
+			} else {
+				board[rowIdx][colIdx] = char
+				colIdx++
+			}
+		}
+	}
+	return board
+}
 
 func main() {
 	chessApp := app.New()
@@ -18,6 +47,7 @@ func main() {
 	window.Resize(fyne.NewSize(600, 600))
 
 	board := container.NewGridWithColumns(boardSize)
+	parsedBoard := parseFEN(strings.Split(startFenNotation, " ")[0])
 
 	for row := 0; row < boardSize; row++ {
 		for col := 0; col < boardSize; col++ {
@@ -27,7 +57,19 @@ func main() {
 			}
 			square := canvas.NewRectangle(squareColor)
 			square.SetMinSize(fyne.NewSize(75, 75))
-			board.Add(square)
+
+			blockHasPiece := parsedBoard[row][col]
+			if blockHasPiece != 0 {
+				imagePath := filepath.Join(pieceDir, mpPieceToImage[blockHasPiece])
+				pieceImage := canvas.NewImageFromFile(imagePath)
+				pieceImage.FillMode = canvas.ImageFillContain
+				pieceImage.Resize(fyne.NewSize(75, 75))
+
+				cell := container.NewStack(square, pieceImage)
+				board.Add(cell)
+			} else {
+				board.Add(square)
+			}
 		}
 	}
 
